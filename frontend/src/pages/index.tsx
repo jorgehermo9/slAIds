323e37de99c963368ca2c8e993ba0287c7ad6ae5
David Rodriguez <davidrbacelar@gmail.com>
2023-02-24 16:33:53 +0100

    Fast committed

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { Carousel } from "@/components/Carousel/Carousel";
 import { ResumeCard } from "@/components/CarouselCards/ResumeCard";
 import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.center}>
-      <ResumeCard />
+      <Carousel labels={["Resume", "Resume"]}>
+        <ResumeCard />
+        <ResumeCard />
+      </Carousel>
     </div>
   );
 }
