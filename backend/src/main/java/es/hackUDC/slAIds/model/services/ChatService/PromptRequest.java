65ddda5a7f1a2dc896e58879db0dbde7050898d2
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2023-02-24 19:54:22 +0100

    fast committed

--- backend/src/main/java/es/hackUDC/slAIds/model/services/ChatService/PromptRequest.java
+++ backend/src/main/java/es/hackUDC/slAIds/model/services/ChatService/PromptRequest.java
@@ -1,6 +1,7 @@
 package es.hackUDC.slAIds.model.services.ChatService;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,25 +14,49 @@ import es.hackUDC.slAIds.model.services.ChatService.PromptResponse.PromptRespons
 
 import java.util.Optional;
 
-record PromptRequest<T>(String text, Class<T> target_class, String conversationId,
+record PromptRequest<T>(String promptText, Class<T> target_class, String conversationId,
         String parentId) {
 
-    public PromptRequest(String text, Class<T> target_class) {
-        this(text, target_class, null, null);
+    public PromptRequest(String promptText, Class<T> target_class) {
+        this(promptText, target_class, null, null);
     }
 
-    public PromptRequest(String text, Class<T> target_class, String conversationId, String parentId) {
-        this.text = text;
+    public PromptRequest(String promptText, Class<T> target_class, String conversationId, String parentId) {
+        String finalPromptText = promptText + "\n" + generateJsonPromptText(target_class);
+        this.promptText = finalPromptText;
         this.target_class = target_class;
         this.conversationId = conversationId;
         this.parentId = parentId;
+    }
+
+    private static <T> String generateJsonPromptText(Class<T> target_class) {
+        String basePrompt = "Your response should be only in JSON format and should have the following keys: ";
+
+        Field[] fields = target_class.getDeclaredFields();
+
+        String fieldsPrompt = "";
+        if (fields.length > 0) {
+            StringBuilder sb = new StringBuilder(basePrompt);
+            for (Field field : fields) {
+                sb.append(field.getName());
+                sb.append(", ");
+            }
+            sb.delete(sb.length() - 2, sb.length());
+            sb.append(".");
+            fieldsPrompt = sb.toString();
+        }
+
+        String endPrompt = "Do not respond anything more than JSON";
+
+        return basePrompt + fieldsPrompt + endPrompt;
+
     }
 
     record PromptRequestDto(String text, @JsonProperty("conversation_id") String conversationId,
             @JsonProperty("parent_id") String parentId) {
 
         public <T> PromptRequestDto(PromptRequest<T> promptRequest) {
-            this(promptRequest.text(), promptRequest.conversationId(), promptRequest.parentId());
+            this(promptRequest.promptText(), promptRequest.conversationId(), promptRequest.parentId());
         }
     }
 
