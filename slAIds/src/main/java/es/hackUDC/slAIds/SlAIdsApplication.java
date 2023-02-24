82a995bd7e768bc899b5cef8ba381841c3320c0c
diego vazquez fernandez <diego.vazquez.fernandez@udc.es>
2023-02-24 12:10:23 +0100

    Creación del proyecto de spring

---
+++ slAIds/src/main/java/es/hackUDC/slAIds/SlAIdsApplication.java
@@ -0,0 +1,13 @@
+package es.hackUDC.slAIds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlAIdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlAIdsApplication.class, args);
+	}
+
+}
---
+++ slAIds/src/test/java/es/hackUDC/slAIds/SlAIdsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.hackUDC.slAIds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlAIdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
