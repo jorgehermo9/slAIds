5388a7be30e0a4e4e4450b3d9fdfe29c436dff3e
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2023-02-25 14:53:56 +0100

    fast committed

---
+++ backend/src/main/java/es/hackUDC/slAIds/model/services/ImageService/ImageServiceStableDiffusion.java
@@ -0,0 +1,12 @@
+package es.hackUDC.slAIds.model.services.ImageService;
+
+import java.util.Optional;
+
+public class ImageServiceStableDiffusion implements ImageService {
+
+    @Override
+    public Optional<byte[]> getImage(String prompt) {
+        return null;
+    }
+
+}
--- stable-diffusion-req.py
+++ stable-diffusion-req.py
@@ -2,6 +2,7 @@ import requests
 import base64
 import io
 from PIL import Image
+
 payload = {
     "prompt": "maltese puppy",
     "batch_size": 1,
@@ -12,10 +13,10 @@ payload = {
 }
 
 
-response = requests.post(url=f'http://127.0.0.1:7860/sdapi/v1/txt2img', json=payload)
+response = requests.post(url=f"http://172.20.10.6:7860/sdapi/v1/txt2img", json=payload)
 
 r = response.json()
 
-for i in r['images']:
-  image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
-  image.show()
+for i in r["images"]:
+    image = Image.open(io.BytesIO(base64.b64decode(i.split(",", 1)[0])))
+    image.show()
