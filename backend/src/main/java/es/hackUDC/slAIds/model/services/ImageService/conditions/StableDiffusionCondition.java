a4083779292d881bd3e56795e54ed2ac66caf0e2
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2023-02-25 15:54:23 +0100

    fast committed

--- backend/src/main/java/es/hackUDC/slAIds/model/services/ImageService/conditions/StableDiffusionCondition.java
+++ backend/src/main/java/es/hackUDC/slAIds/model/services/ImageService/conditions/StableDiffusionCondition.java
@@ -1,4 +1,4 @@
-package es.hackUDC.slAIds.model.services.ImageService;
+package es.hackUDC.slAIds.model.services.ImageService.conditions;
 
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
